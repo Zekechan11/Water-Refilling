f8cef904f8a6229865a558f3958df2967d8987b2
Zeke <145641228+Zekechan11@users.noreply.github.com>
2024-10-13 18:32:11 +0800

    customer dashboard

--- src/views/agent/Dashboard.vue
+++ src/views/agent/Dashboard.vue
@@ -5,9 +5,7 @@ import { onBeforeMount, ref } from 'vue';
 const { getPrimary, getSurface, isDarkTheme } = useLayout();
 const customers2 = ref(null);
 
-onBeforeMount(() => {
-
-});
+onBeforeMount(() => {});
 
 const formatCurrency = (value) => {
   return value.toLocaleString("en-US", { style: "currency", currency: "PHP" });
@@ -16,42 +14,42 @@ const formatCurrency = (value) => {
 
 <template>
   <div class="space">
-    <h1 class="text-4xl font-bold mb-6" style="color: #899499;">Container on Loan</h1>
+    <h1 class="text-2xl md:text-4xl font-bold mb-6 text-gray-500">Container on Loan</h1>
   </div>
 
-  <div class="card shadow-md" style="height: 550px">
+  <div class="card shadow-md h-auto md:h-550px p-4">
     <!-- Tab content -->
     <div>
       <center>
-        <h1 class="text-4xl font-bold mb-6" style="color: #899499;">Guadalupe</h1>
+        <h1 class="text-2xl md:text-4xl font-bold mb-6 text-gray-500">Guadalupe</h1>
       </center>
-      <ul class="list-none p-0 m-0 mt-10 px-10">
+
+      <ul class="list-none p-0 m-0 mt-10 px-4 md:px-10">
         <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6">
-          <div style="background-color: #007fff;padding: 20px;border-radius: 10px;color: white;">
-            <span class="font-bold mb-1 md:mb-0"
-              style="font-size: 24px;margin-right: 720px">Container on loan :</span>
-            <span class="ml-8 font-medium" style="font-size: 24px;">50</span>
+          <div class="bg-blue-500 p-5 rounded-lg text-white flex justify-between w-full">
+            <span class="font-bold text-lg md:text-2xl">Container on loan:</span>
+            <span class="ml-8 font-medium text-lg md:text-2xl">50</span>
           </div>
         </li>
+
         <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6">
-          <div style="background-color: #007fff;padding: 20px;border-radius: 10px; color: white;">
-            <span class="font-bold mb-1 md:mb-0"
-              style="font-size: 24px;margin-right: 720px">Gallons Delivered :</span>
-            <span class="ml-8 font-medium" style="font-size: 24px;">50</span>
+          <div class="bg-blue-500 p-5 rounded-lg text-white flex justify-between w-full">
+            <span class="font-bold text-lg md:text-2xl">Gallons Delivered:</span>
+            <span class="ml-8 font-medium text-lg md:text-2xl">50</span>
           </div>
         </li>
+
         <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6">
-          <div style="background-color: #007fff;padding: 20px;border-radius: 10px;color: white;">
-            <span class="font-bold mb-1 md:mb-0"
-              style="font-size: 24px;margin-right: 720px">Gallons Collected :</span>
-            <span class="ml-8 font-medium" style="font-size: 24px;">50</span>
+          <div class="bg-blue-500 p-5 rounded-lg text-white flex justify-between w-full">
+            <span class="font-bold text-lg md:text-2xl">Gallons Collected:</span>
+            <span class="ml-8 font-medium text-lg md:text-2xl">50</span>
           </div>
         </li>
+
         <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6">
-          <div style="background-color: #007fff;padding: 20px;border-radius: 10px;color: white;">
-            <span class="font-bold mb-1 md:mb-0"
-              style="font-size: 24px;margin-right: 720px">Amount Collected:</span>
-            <span class="ml-8 font-medium" style="font-size: 24px;">50</span>
+          <div class="bg-blue-500 p-5 rounded-lg text-white flex justify-between w-full">
+            <span class="font-bold text-lg md:text-2xl">Amount Collected:</span>
+            <span class="ml-8 font-medium text-lg md:text-2xl">₱50</span>
           </div>
         </li>
       </ul>
--- src/views/user/Dashboard.vue
+++ src/views/user/Dashboard.vue
@@ -8,12 +8,12 @@ const dateFrozen = ref(false);
 const customers2 = ref(null);
 
 onBeforeMount(() => {
-    CustomerService.getCustomersLarge().then((data) => (customers2.value = data));
+  CustomerService.getCustomersLarge().then((data) => (customers2.value = data));
 
 });
 
 function formatCurrency(value) {
-    return value.toLocaleString('en-US', { style: 'currency', currency: 'PHP' });
+  return value.toLocaleString('en-US', { style: 'currency', currency: 'PHP' });
 }
 </script>
 
@@ -22,66 +22,62 @@ function formatCurrency(value) {
     <h1 class="text-4xl font-bold mb-6" style="color: #899499;">Dashboard</h1>
   </div>
 
-  <div class="card shadow-md">
-    <!-- <div class="flex justify-between items-center mb-6">
-
-      <div class="flex space-x-4">
-        <button
-          :class="['py-2 px-4 rounded', activeTab === 'purchase' ? 'bg-primary text-white' : 'bg-gray-200']"
-          @click="activeTab = 'purchase'"
-        >
-          Purchase
-        </button>
-        <button
-          :class="['py-2 px-4 rounded', activeTab === 'utang' ? 'bg-primary text-white' : 'bg-gray-200']"
-          @click="activeTab = 'utang'"
-        >
-          History
-        </button>
-      </div>
-    </div> -->
-
-    <!-- Tab content -->
+  <div class="card shadow-md  ">
     <div>
       <ul class="list-none p-0 m-0">
-        <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6">
-          <div>
+        <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6 bg-blue-100 p-4 rounded-md" style="font-size: 18px;">
+          
+          <div class="mt-2 md:mt-0 flex items-center">
             <span class="text-surface-900 dark:text-surface-0 font-bold mr-2 mb-1 md:mb-0">Area :</span>
           </div>
-          <div class="mt-2 md:mt-0 ml-0 md:ml-20 flex items-center">
+
+          <div class="mt-2 md:mt-0 flex items-center">
             <span class="ml-4 font-medium">Guadalupe</span>
           </div>
-          <div>
+
+          <div class="mt-2 md:mt-0 flex items-center">
             <span class="text-surface-900 dark:text-surface-0 font-bold mr-2 mb-1 md:mb-0">Agent :</span>
           </div>
-          <div class="mt-2 md:mt-0 ml-0 md:ml-20 flex items-center">
+
+          <div class="mt-2 md:mt-0 flex items-center">
             <span class="ml-4 font-medium">Zeke</span>
           </div>
         </li>
 
-        <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-12">
-          <div>
+        <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6 bg-green-100 p-4 rounded-md" style="font-size: 18px;">
+
+          <div class="mt-2 md:mt-0 flex items-center">
             <span class="text-surface-900 dark:text-surface-0 font-bold mr-2 mb-1 md:mb-0">Gallons on Hold :</span>
           </div>
+
           <div class="mt-2 md:mt-0 flex items-center">
             <span class="ml-4 font-medium">50</span>
           </div>
-          <div>
+
+          <div class="mt-2 md:mt-0 flex items-center">
             <span class="text-surface-900 dark:text-surface-0 font-bold mr-2 mb-1 md:mb-0">Date :</span>
           </div>
+
           <div class="mt-2 md:mt-0 ml-0 md:ml-20 flex items-center">
             <span class="ml-4 font-medium">02/11/2020</span>
           </div>
         </li>
-        <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6">
-          <div>
+
+        <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-12 bg-yellow-100 p-4 rounded-md" style="font-size: 18px;">
+
+          <div class="mt-2 md:mt-0 flex items-center">
             <span class="text-surface-900 dark:text-surface-0 font-bold mr-2 mb-1 md:mb-0">Purchase Gallons:</span>
           </div>
+
           <div class="mt-2 md:mt-0 ml-0 md:ml-20 flex items-center">
             <span class="ml-4 font-medium">{{ formatCurrency(16) }}</span>
           </div>
+
         </li>
-        <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-12">
+
+
+        <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6 bg-pink-100 p-4 rounded-md" style="font-size: 18px;">
+          
           <div>
             <span class="text-surface-900 dark:text-surface-0 font-bold mr-2 mb-1 md:mb-0">Amount Paid :</span>
           </div>
@@ -89,57 +85,41 @@ function formatCurrency(value) {
             <span class="ml-4 font-medium">{{ formatCurrency(16) }}</span>
           </div>
         </li>
-        <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6">
+
+        <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6 bg-indigo-100 p-4 rounded-md" style="font-size: 18px;">
           <div>
-            <span class="text-surface-900 dark:text-surface-0 font-bold mr-2 mb-1 md:mb-0">Amount Payable  :</span>
+            <span class="text-surface-900 dark:text-surface-0 font-bold mr-2 mb-1 md:mb-0">Amount Payable :</span>
           </div>
           <div class="mt-2 md:mt-0 ml-0 md:ml-20 flex items-center">
             <span class="ml-4 font-medium">{{ formatCurrency(20) }}</span>
           </div>
         </li>
-        <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6">
+
+        <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6 bg-purple-100 p-4 rounded-md" style="font-size: 18px;">
           <div>
-            <span class="text-surface-900 dark:text-surface-0 font-bold mr-2 mb-1 md:mb-0">Paid  :</span>
+            <span class="text-surface-900 dark:text-surface-0 font-bold mr-2 mb-1 md:mb-0">Paid :</span>
           </div>
           <div class="mt-2 md:mt-0 ml-0 md:ml-20 flex items-center">
             <span class="ml-4 font-medium">{{ formatCurrency(20) }}</span>
           </div>
         </li>
-        
+
       </ul>
     </div>
-
-    <!-- <div v-if="activeTab === 'utang'">
-        <div class="font-semibold text-xl mb-4">Frozen Columns</div>
-    <ToggleButton v-model="dateFrozen" onIcon="pi pi-lock" offIcon="pi pi-lock-open" onLabel="Date" offLabel="Date" />
-    <DataTable :value="customers2" scrollable scrollHeight="400px" class="mt-6">
-      <Column field="id" header="Id" style="min-width: 100px"></Column>
-      <Column field="activity" header="Gallons on Hold" style="min-width: 200px"></Column>
-      <Column field="activity" header="Purchase" style="min-width: 200px"></Column>
-      <Column field="balance" header="Utang" style="min-width: 200px">
-        <template #body="{ data }">
-          <span>{{ formatCurrency(data.balance) }}</span>
-        </template>
-      </Column>
-      <Column field="country.name" header="Area" style="min-width: 200px"></Column>
-      <Column field="representative.name" header="Agent" style="min-width: 200px"></Column>
-      <Column field="date" header="Date" style="min-width: 200px" alignFrozen="right" :frozen="dateFrozen"></Column>
-    </DataTable>
-
-    </div> -->
   </div>
+
 
   <!-- DataTable Card for Customers -->
   <div class="card shadow-md">
-        <ToggleButton v-model="dateFrozen" onIcon="pi pi-lock" offIcon="pi pi-lock-open" onLabel="Date" offLabel="Date" />
+    <ToggleButton v-model="dateFrozen" onIcon="pi pi-lock" offIcon="pi pi-lock-open" onLabel="Date" offLabel="Date" />
 
-        <DataTable :value="customers2" scrollable scrollHeight="400px" class="mt-6">
-            <Column field="activity" header="Purchase Gallons" style="min-width: 200px"></Column>
-            <Column field="representative.name" header="Galllons Collected" style="min-width: 200px"></Column>
-            <Column field="representative.name" header="Amount Paid" style="min-width: 200px"></Column>
-            <Column field="date" header="Date" style="min-width: 200px" alignFrozen="right" :frozen="dateFrozen"></Column>
-            <!-- <Column field="status" header="Status" style="min-width: 200px"></Column> -->
-       
-        </DataTable>
-    </div>
+    <DataTable :value="customers2" scrollable scrollHeight="400px" class="mt-6">
+      <Column field="activity" header="Purchase Gallons" style="min-width: 200px"></Column>
+      <Column field="representative.name" header="Galllons Collected" style="min-width: 200px"></Column>
+      <Column field="representative.name" header="Amount Paid" style="min-width: 200px"></Column>
+      <Column field="date" header="Date" style="min-width: 200px" alignFrozen="right" :frozen="dateFrozen"></Column>
+      <!-- <Column field="status" header="Status" style="min-width: 200px"></Column> -->
+
+    </DataTable>
+  </div>
 </template>
